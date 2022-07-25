27fa918b470793bf9607f586da48afbec3f9fe45
IGAP <gatadeabhijit@gmail.com>
2022-07-25 07:42:30 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'second-project';
+  enabled:boolean = false;
+  name:string = "Abhijit Gatade";
+  choice:number = 1;
+
+  months = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+
+  ngOnInit(): void {
+  setInterval(()=>{
+    this.enabled = !this.enabled;
+  }, 3000);
+  }
+
+  clicked(){
+    alert("You clicked Button");
+  }
 }
